fdf54abf75facc9620f0402626194ed7c39ea4f9
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-06 18:37:07 -0700

    Adding MIT license.

--- observer-design-pattern.js
+++ observer-design-pattern.js
@@ -1,15 +1,19 @@
 /**
  * @file
- * Observer design pattern.
- *
- * This object will provide an observer design pattern. Observers may
- * subscribe to specific message types. Observables may broadcast
- * messages specific to a type.
- *
- * @author Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
+ * @author  Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
  * @version 0.1
  *
- * @see http://www.joezimjs.com/javascript/javascript-design-patterns-observer/
+ * @copyright Copyright (c) 2013 Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
+ * Released under the MIT license. Read the entire license located in the
+ * project root or at http://opensource.org/licenses/mit-license.php
+ *
+ * @brief Observer design pattern.
+ *
+ * @details This object will provide an observer design pattern. Observers may
+ * subscribe to specific message types. Observables may broadcast messages
+ * specific to a type.
+ *
+ * @see https://github.com/delphian/design-patterns
  */
 
 /**
